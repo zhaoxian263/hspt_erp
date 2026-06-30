739b61bbe5dc8294309aeb92aded1d0db6931e12
zhaoxian263 <wangyi7575@126.com>
2026-06-30 10:19:14 +0800

    chore: 上传文件大小限制从16MB调整为64MB

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = os.environ.get('ERP_SECRET_KEY', os.urandom(24).hex())
 
-# 上传文件大小限制 16MB
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+# 上传文件大小限制64MB
+MAX_CONTENT_LENGTH = 64 * 1024 * 1024
 
 # 分页默认值
 PAGE_SIZE = 20
